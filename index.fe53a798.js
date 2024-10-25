b062ed031f31628cc2278128b7ef279190535d76
Ivan Paiareli <ipayareli@gmail.com>
2024-10-25 18:52:07 +0300

    make build

---
+++ index.fe53a798.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".spider");t.style.top="50%",t.style.left="50%",t.style.position="absolute",t.style.transform="translate(-50%, -50%)";
+//# sourceMappingURL=index.fe53a798.js.map
